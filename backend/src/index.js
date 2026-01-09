45f947fc690da234015ca12bf928749c15fb90b6
AnusreeE719 <143895536+AnusreeE719@users.noreply.github.com>
2026-01-10 03:55:18 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/authRoutes.js';
 import todoRoutes from './routes/todoRoutes.js'
@@ -9,20 +10,40 @@ import todoRoutes from './routes/todoRoutes.js'
 import { connectDB } from './config/db.js';
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
 
 dotenv.config();
 const PORT = process.env.PORT || 2026;
 
+const __dirname = path.resolve();
+
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+    })
+  );
+}
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", todoRoutes);
 
-app.get('/', (req, res) => {
-    console.log(req);
-    return res.status(234).send('Welcome to MERN stack todo project');
-})
+if (process.env.NODE_ENV === "production") {
+  // Serve static build
+  app.use(express.static(path.join(__dirname, "Frontend", "dist")));
+
+  // Any non-API route -> serve index.html
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+  });
+}
+
+// app.get('/', (req, res) => {
+//     console.log(req);
+//     return res.status(234).send('Welcome to MERN stack todo project');
+// })
 
 app.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
